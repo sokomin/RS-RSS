7d9de686be73b302e1ca95dca0b7661d3f8aeeab
sokomin <sokoranominsyu@gmail.com>
2023-03-11 23:32:16 +0900

    ログ追加

--- get_timestamp.py
+++ get_timestamp.py
@@ -2,9 +2,9 @@ import time
 ###
 # 2か月に1度コミットしないとRSS取得が停止してしまうので、timestampをコミットしてpushするためのプログラム
 ###
-
 # 現在のタイムスタンプを取得
 timestamp = int(time.time())
+print(f"update timestamp... {timestamp}")
 
 # タイムスタンプを文字列に変換
 timestamp_str = str(timestamp)
